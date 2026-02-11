d8793f71eaa6e5fd1deaa48fad4ab4230f56243b
atchyut96379 <atchyut96379@gmail.com>
2026-02-11 14:40:27 +0530

    Added uvicorn run config

--- GatePass_App/backend/main.py
+++ GatePass_App/backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from auth import router as auth_router
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 app = FastAPI()
 
@@ -12,3 +13,6 @@ app.add_middleware(
 )
 
 app.include_router(auth_router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
