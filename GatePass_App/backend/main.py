48f2e559256692ec8e9753ea2753ee9936e8966c
atchyut96379 <atchyut96379@gmail.com>
2026-02-11 15:13:11 +0530

    Testing lines are added in main

--- GatePass_App/backend/main.py
+++ GatePass_App/backend/main.py
@@ -16,3 +16,7 @@ app.include_router(auth_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
+
+@app.get("/")
+def home():
+    return {"message": "Gate Pass System Backend is Running 🚀"}
