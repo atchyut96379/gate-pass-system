cc88d3adbee9fb6da481132a4e785dfef4955c0d
atchyut96379 <atchyut96379@gmail.com>
2026-02-11 16:37:51 +0530

    Modifying the db details for render

--- GatePass_App/backend/db.py
+++ GatePass_App/backend/db.py
@@ -1,35 +1,11 @@
 import os
 import psycopg2
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+def get_connection():
+    database_url = os.getenv("DATABASE_URL")
 
-conn = psycopg2.connect(DATABASE_URL)
-conn.autocommit = True
+    if not database_url:
+        raise Exception("DATABASE_URL not set!")
 
-cursor = conn.cursor()
-
-def create_tables():
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS users (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(100),
-            email VARCHAR(100) UNIQUE,
-            password VARCHAR(100),
-            phone VARCHAR(20),
-            flat_number VARCHAR(20),
-            role VARCHAR(20)
-        );
-    """)
-
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS visitors (
-            id SERIAL PRIMARY KEY,
-            visitor_name VARCHAR(100),
-            visitor_phone VARCHAR(20),
-            purpose TEXT,
-            flat_number VARCHAR(20),
-            otp VARCHAR(6),
-            status VARCHAR(20),
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        );
-    """)
+    conn = psycopg2.connect(database_url)
+    return conn
